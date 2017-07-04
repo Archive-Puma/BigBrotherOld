6902aca17a5dc361151dfe450c9f5a9533242faf
loro <puma@localhost.localdomain>
2017-07-04 13:32:08 +0200

    Body-Parser

--- pug.js
+++ pug.js
@@ -1,11 +1,38 @@
+/* ------------------------------------ */
+/* ------------- LIBRARIES ------------ */
+/* ------------------------------------ */
+
 // Import Express library
 var express = require("express");
+
+// Import Body-Parser library
+var bodyparser = require("body-parser");
+
+/* ------------------------------------ */
+/* -------------- OPTIONS ------------- */
+/* ------------------------------------ */
 
 // Create server
 var server = express();
 
-// Set view
+// Set ViewEngine to read Pug views
 server.set("view engine","pug");
+
+// Argument Parser: Application/JSON
+server.use(bodyparser.json());
+// Argument Parser: Text/HTML
+server.use(bodyparser.urlencoded({extended:true }));
+
+/* ------------------------------------ */
+/* ------------ MIDDLEWARES ----------- */
+/* ------------------------------------ */
+
+// Set link to static files
+server.use("/assets/public_files", express.static('public'));
+
+/* ------------------------------------ */
+/* ------------- REQUESTS ------------- */
+/* ------------------------------------ */
 
 // Root Dir
 server.get("/", function(request, response) {
@@ -13,20 +40,17 @@ server.get("/", function(request, response) {
     response.render("index");
 });
 
-// Dir with params
-server.get("/:name", function(request, response) {
-    // Read Form with params
-    console.log(request.params.name)
-    response.render("form", {
-        name: request.params.name
-    });
-});
-
-
 // Root Form Request
 server.post("/", function(request,response) {
-    response.render("form");
+    console.log("Name: " + request.body.name);
+    console.log("Surname: " + request.body.surname);
+    console.log("Nickname: " + request.body.nickname);
+    response.send("Datos recibidos");
 });
+
+/* ------------------------------------ */
+/* -------------- THREAD -------------- */
+/* ------------------------------------ */
 
 // Start server in port 8080
 server.listen(8080);
