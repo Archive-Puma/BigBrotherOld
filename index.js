5ef0300d890620c31e1d2c7f17d269bf009be649
CosasDePuma <kikedesarrollador@gmail.com>
2017-07-05 00:27:17 +0200

    Mongoose

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 /* ------------- LIBRARIES ------------ */
 /* ------------------------------------ */
 
+// Import Body-Parser library
+var bodyparser = require("body-parser");
+
 // Import Express library
 var express = require("express");
 
-// Import Body-Parser library
-var bodyparser = require("body-parser");
+// Import Mongoose library
+var mongo = require("mongoose");
 
 /* ------------------------------------ */
 /* -------------- OPTIONS ------------- */
@@ -14,6 +17,28 @@ var bodyparser = require("body-parser");
 
 // Create server
 var server = express();
+
+// Connect to MongoDB
+mongo.connect("mongodb://localhost/bigbrother", { useMongoClient: true });
+
+// Create MongoDB Schemas
+
+var twitter_user_schema = new mongo.Schema({
+    userID:String,
+    user:String,
+    name:String,
+    location:String,
+    description:String,
+    url:String,
+    protected:Boolean,
+    following:Number,
+    followers:Number,
+    lang:String
+});
+
+// Create MongoDB User
+
+var twitter_user = mongo.model("twitter_user", twitter_user_schema);
 
 // Set ViewEngine to read Pug views
 server.set("view engine","pug");
@@ -44,6 +69,56 @@ server.get("/", function(request, response) {
 server.post("/", function(request,response) {
     console.log("Name: " + request.body.name + "\nSurname: " + request.body.surname + "\nNickname: " + request.body.nickname);
     response.send("Datos recibidos");
+});
+
+// Root Dir
+server.get("/twitter", function(request, response) {
+    // Read database
+    twitter_user.find(function(error, document) {
+        response.send(document);
+    });
+});
+
+// Twitter Information
+server.post("/twitter", function(request, response) {
+    // Import Twit library
+    var Twitter = require("twit");
+    // Set keys and tokens
+    var T = new Twitter({
+        consumer_key:         'H54twKyiED5NrHmqrDI1HtKFv',
+        consumer_secret:      'zPL3MU5eAJSO5N7WKdqvuXHUyF8euYGxEb1g5Bd1k7rjqzIKP7',
+        access_token:         '832570595917316097-svywXbVnQK1rcznLkqN69MTzAYwH0Zj',
+        access_token_secret:  'YftzBi3ZjFJxDb2myjwiyi6ntBl7qiJAvQCC57AjabGEN',
+    });
+
+    // Search Twitter users
+    T.get("users/search", {
+        "q": request.body.name,
+        count: 5
+    }, function(error,data,res) {
+        for(var profile in data) {
+            // Fill user information
+            var user = new twitter_user({
+                userID:data[profile]['id_str'],
+                user:data[profile]['screen_name'],
+                name:data[profile]['name'],
+                location:data[profile]['location'],
+                description:data[profile]['description'],
+                url:data[profile]['url'],
+                protected:data[profile]['protected'],
+                following:data[profile]['friends_count'],
+                followers:data[profile]['followers_count'],
+                lang:data[profile]['lang']
+            });
+            // Save user info in database
+            user.save(function(){
+                return;
+            });
+        }
+        return;
+    });
+
+    response.send("Información recibida!");
 });
 
 /* ------------------------------------ */
