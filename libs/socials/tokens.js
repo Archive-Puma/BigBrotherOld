8bf657526ef3fbfd4922706234d4cbb0ecf60b5a
CosasDePuma <kikedesarrollador@gmail.com>
2017-07-11 13:47:41 +0200

    TOKEN MANAGEMENT

---
+++ libs/socials/tokens.js
@@ -0,0 +1,9 @@
+// TWITTER ~ https://apps.twitter.com/
+var Twitter = new require("twit")({
+    consumer_key:         '',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  '',
+});
+
+module.exports.Twitter = Twitter;
--- libs/socials/twitter.js
+++ libs/socials/twitter.js
@@ -1,13 +1,4 @@
-// Import Twit library
-var T = require("twit");
-
-// Set keys and tokens
-var Twitter = new T({
-    consumer_key:         'H54twKyiED5NrHmqrDI1HtKFv',
-    consumer_secret:      'zPL3MU5eAJSO5N7WKdqvuXHUyF8euYGxEb1g5Bd1k7rjqzIKP7',
-    access_token:         '832570595917316097-svywXbVnQK1rcznLkqN69MTzAYwH0Zj',
-    access_token_secret:  'YftzBi3ZjFJxDb2myjwiyi6ntBl7qiJAvQCC57AjabGEN',
-});
+var Twitter = require("./tokens").Twitter;
 
 // Schema 
 var twitter_user = require("../models/twitter/user").twitter_user;
@@ -52,5 +43,4 @@ function twitter_search(session_id, name) {
     return;
 };
 
-module.exports.Twitter = T;
 module.exports.twitter_search = twitter_search;
