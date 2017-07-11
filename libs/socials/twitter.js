eb2249cf2a1345ea85973e39eb1923d544205d5d
CosasDePuma <kikedesarrollador@gmail.com>
2017-07-11 20:38:24 +0200

    Twitter User

--- libs/socials/twitter.js
+++ libs/socials/twitter.js
@@ -5,7 +5,6 @@ var twitter_user = require("../models/twitter/user").twitter_user;
 
 // Functions
 function twitter_search(session_id, name) {
-    console.log(session_id);
     Twitter.get("users/search", {
         "q": name,
         count: 5
