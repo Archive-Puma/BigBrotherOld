d5e81a79bca17bbdff71390b6983dac8e98af278
Cosas de Puma <kikefontanlorenzo@gmail.com>
2018-04-25 14:37:19 +0200

    Form Handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 if(LICENSE['611672b8b8bec6851024b7f26f7434ca'] === 'c9ecf1907bea67f9151f7f37947337e3') {
-  document.getElementById('search-btn').addEventListener('click', () => {
+  document.getElementById('search').addEventListener('submit', () => {
+      event.preventDefault();
       let searchers = remote.require('./src/lib/js/scrapers.js')
-      searchers.DuckDuckGo(document.getElementById('target').value)
+      searchers.DuckDuckGo(document.getElementById('name').value)
       while(!searchers.Resultados.DuckDuckGo);
       console.log(searchers.Resultados)
+      return false
   })
 } else remote.getCurrentWindow().close()
