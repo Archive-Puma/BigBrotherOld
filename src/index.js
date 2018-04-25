ee3997794891f253f5d2aa0e5a03fcc463a4a070
Cosas de Puma <kikefontanlorenzo@gmail.com>
2018-04-25 05:56:56 +0200

    Licenses and Buttons

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-
-// https://medium.com/data-scraper-tips-tricks/scraping-data-with-javascript-in-3-minutes-8a7cf8275b31
-
-document.getElementById('search-btn').addEventListener('click', () => {
-    let searchers = remote.require('./src/lib/js/scrapers.js')
-    searchers.DuckDuckGo(document.getElementById('target').value)
-    while(!searchers.Resultados.DuckDuckGo);
-    console.log(searchers.Resultados)
-})
+if(LICENSE['611672b8b8bec6851024b7f26f7434ca'] === 'c9ecf1907bea67f9151f7f37947337e3') {
+  document.getElementById('search-btn').addEventListener('click', () => {
+      let searchers = remote.require('./src/lib/js/scrapers.js')
+      searchers.DuckDuckGo(document.getElementById('target').value)
+      while(!searchers.Resultados.DuckDuckGo);
+      console.log(searchers.Resultados)
+  })
+} else remote.getCurrentWindow().close()
